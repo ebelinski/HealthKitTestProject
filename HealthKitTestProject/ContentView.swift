d0a0f5edaff6e5bdecfd93362440db65e67a700a
Eugene Belinski <eugenemsp@gmail.com>
2022-04-27 08:46:05 -0500

    Added HealthKit request for step counts.

--- HealthKitTestProject/ContentView.swift
+++ HealthKitTestProject/ContentView.swift
@@ -1,16 +1,31 @@
-//
-//  ContentView.swift
-//  HealthKitTestProject
-//
-//  Created by Eugeniu Belinski on 4/27/22.
-//
-
 import SwiftUI
+import HealthKit
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("Request HealthKit Access") {
+            requestHealthKitAccess()
+        }
+    }
+
+    func requestHealthKitAccess() {
+        guard HKHealthStore.isHealthDataAvailable() else {
+            return
+        }
+
+        guard let stepCount = HKObjectType.quantityType(forIdentifier: .stepCount) else {
+            return
+        }
+
+        let healthKitTypesToRead: Set<HKObjectType> = [stepCount]
+
+        HKHealthStore().requestAuthorization(toShare: [], read: healthKitTypesToRead) {
+            success, error in
+            print(success)
+            if let error = error {
+                print(error.localizedDescription)
+            }
+        }
     }
 }
 
