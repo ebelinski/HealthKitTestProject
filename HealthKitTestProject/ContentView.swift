6c15153074dff940dcae3bbf056778509f932c40
Eugene Belinski <eugenemsp@gmail.com>
2022-04-27 08:34:48 -0500

    Initial Commit

---
+++ HealthKitTestProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HealthKitTestProject
+//
+//  Created by Eugeniu Belinski on 4/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HealthKitTestProject/HealthKitTestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthKitTestProjectApp.swift
+//  HealthKitTestProject
+//
+//  Created by Eugeniu Belinski on 4/27/22.
+//
+
+import SwiftUI
+
+@main
+struct HealthKitTestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
